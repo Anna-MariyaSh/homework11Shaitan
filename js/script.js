9b41c1973419f31dc107b3dc22959a37dfb32f07
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-10-14 20:14:37 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let number = +prompt("Put your number");
+for(let i = 2; i < number; i++) {
+  if(number % i === 0) {
+   console.log("false");
+   break;
+  } else {
+   console.log("true");
+   break;
+  }
+}
