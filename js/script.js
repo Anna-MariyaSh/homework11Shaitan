afc90f3746b0fc255a3d645c8133a2a2208a34f7
Anna-MariyaSh <88891571+Anna-MariyaSh@users.noreply.github.com>
2021-10-16 22:09:44 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-let number = +prompt("Put your number");
-for(let i = 2; i < number; i++) {
-  if(number % i === 0) {
-   console.log("false");
-   break;
-  } else {
-   console.log("true");
+const number = +prompt("Put your number");
+if(isNaN(number)) {
+  alert("incorrect value");
+} else {
+  for(let i = 2; i < number; i++) {
+    if(number % i === 0) {
+     console.log("false");
+     braek;
+    }
   }
+   console.log("true");
 }
